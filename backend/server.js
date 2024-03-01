92726d94f9a7c63e7ac3e8c2f9ba69dea03e04ef
Vphundekar02 <vphundekar02@gmail.com>
2024-03-01 11:19:19 +0530

    Backend 1st part completed

---
+++ backend/server.js
@@ -0,0 +1,32 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
+import authRoutes from "./routers/auth.routes.js";
+import messageRoutes from "./routers/message.routes.js";
+import userRoutes from "./routers/user.routes.js";
+
+import connectToMongoDB from "./db/connectToMongoDB.js";
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+dotenv.config();
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
+
+
+// app.get("/", (req,res) => {
+//     res.send('Hi I am Vinay');
+// });
+
+
+app.listen(port, () => {
+    connectToMongoDB();
+    console.log(`Server is running on port : ${port}`);
+});
