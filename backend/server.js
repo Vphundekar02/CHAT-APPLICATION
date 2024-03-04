0dc17be6de223486977f7ff11fb393bf7bb9b8c4
Vphundekar02 <vphundekar02@gmail.com>
2024-03-04 22:52:10 +0530

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-
+import path from "path";
 import authRoutes from "./routers/auth.routes.js";
 import messageRoutes from "./routers/message.routes.js";
 import userRoutes from "./routers/user.routes.js";
@@ -12,6 +12,7 @@ import { app, server } from "./socket/socket.js";
 // const app = express();
 const port = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
 dotenv.config();
 
 app.use(express.json());
@@ -21,6 +22,11 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+})
 
 // app.get("/", (req,res) => {
 //     res.send('Hi I am Vinay');
